12ed7eafd5eec504c64a5d81b9077706f198f9ca
samuelmoiwa <samuelmoiwa@gmail.com>
2023-05-26 19:26:50 +0000

    update info

--- src/js/typed.js
+++ src/js/typed.js
@@ -1,7 +1,9 @@
 import Typed from 'typed.js'
 
 var options = {
-  strings: ["I'm a Front-end Developer", 'I Build  Websites', "I Solve Problems"],
+  strings: ["I'm a Health and Policy Professional",
+            'I worked on public health issues in the US, Rwanda, and the UK',
+            "My work includes program management, qualitative and quantitative research analysis"],
   typeSpeed: 120,
   backSpeed: 50,
   backDelay: 1500,
